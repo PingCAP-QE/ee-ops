16a93eec3fa08846165c3d7e555343afb49e92d9
wuhuizuo <wuhuizuo@126.com>
2022-07-11 20:08:53 +0800

    chore(.ci/scripts): add compose logic for trigger template

--- .ci/scripts/compose.ts
+++ .ci/scripts/compose.ts
@@ -1,12 +1,59 @@
 // init compose script
-import { parseAll } from "https://deno.land/std@0.147.0/encoding/yaml.ts";
+import { parse as parseYaml, stringify as stringifyYaml } from "https://deno.land/std@0.147.0/encoding/yaml.ts";
+
+interface ciFileBlob {
+    file_sha: string;
+    path: string;
+    content: string;
+}
+
+interface pipelineRun { [key: string]: any }
+interface triggerTemplate { [key: string]: any }
 
 async function readManifest(path: string) {
     const decoder = new TextDecoder("utf-8");
 
     const data = await Deno.readFile(path);
-    const result = parseAll(decoder.decode(data));
-    console.debug(result);
+    const result = parseYaml(decoder.decode(data)) as ciFileBlob[];
+
+    result.forEach(e => {
+        const ePipelineRun = parseYaml(e.content) as pipelineRun;
+        const eTriggerTemplate = composeTriggerTemplate(ePipelineRun);
+
+        console.log(stringifyYaml(eTriggerTemplate))
+    });
+}
+
+function composeTriggerTemplate(run: pipelineRun): triggerTemplate {
+    const ret: triggerTemplate = {
+        apiVersion: 'triggers.tekton.dev/v1beta1',
+        kind: 'TriggerTemplate',
+        metadata: {
+            name: 'github-template-owner-repo-pr-12345',
+            labels: {
+                type: 'github-pr',
+                prNum: 12345,
+                prOwner: 'PingCAP-QE',
+                prRepo: 'ee-ops',
+            }
+        },
+        spec: {
+            params: [
+                {
+                    name: 'git-url',
+                    description: 'The git repository full url'
+                },
+                {
+                    name: 'git-revision',
+                    default: 'main',
+                    description: 'The git revision'
+                }
+            ],
+            resourcetemplates: [run]
+        }
+    };
+
+    return ret;
 }
 
 await readManifest('ci.yaml');
