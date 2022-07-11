6db2483e9b5b80c66010bcda9bea5f8e54bb4c48
wuhuizuo <wuhuizuo@126.com>
2022-07-11 19:39:59 +0800

    chore(.ci/scripts): add read logic

--- .ci/scripts/compose.ts
+++ .ci/scripts/compose.ts
@@ -1,1 +1,12 @@
 // init compose script
+import { parseAll } from "https://deno.land/std@0.147.0/encoding/yaml.ts";
+
+async function readManifest(path: string) {
+    const decoder = new TextDecoder("utf-8");
+
+    const data = await Deno.readFile(path);
+    const result = parseAll(decoder.decode(data));
+    console.debug(result);
+}
+
+await readManifest('ci.yaml');
