4271d0ca7c02f2b75e670acba9966d514b37df9f
wuhuizuo <wuhuizuo@126.com>
2022-07-12 19:14:27 +0800

    feat(apps/staging/tekton/configs/pipelines): add trigger composing logic

--- .ci/scripts/compose.ts
+++ .ci/scripts/compose.ts
@@ -1,5 +1,6 @@
 import * as yaml from "https://deno.land/std@0.147.0/encoding/yaml.ts";
 import * as flags from "https://deno.land/std@0.147.0/flags/mod.ts";
+import { sprintf } from "https://deno.land/std@0.147.0/fmt/printf.ts";
 
 const DEFAULT_CI_MANIFEST_INPUT = "ci.yaml"
 
@@ -9,19 +10,49 @@ interface ciFileBlob {
     content: string;
 }
 
-interface cliParams {
-    input?: string; // default ci.yaml
+interface prInfo {
+    number: number
+    user: string
+    headOwner: string
+    headRepo: string
+    headRef: string
+    baseOwner: string
+    baseRepo: string
+    baseRef: string
 }
 
+interface cliParams {
+    input?: string // default ci.yaml
+    pr: prInfo
+}
+
+
 interface pipelineRun { [key: string]: any }
-interface triggerTemplate { [key: string]: any }
+interface triggerTemplate {
+    kind: "TriggerTemplate"
+    metadata: {
+        name: string
+        [key: string]: any
+    },
+    spec: { [key: string]: any }
+    [key: string]: any
+}
+interface trigger {
+    kind: "Trigger"
+    metadata: {
+        name: string
+        [key: string]: any
+    },
+    spec: { [key: string]: any }
+    [key: string]: any
+}
 
 function readManifest(path: string) {
     const content = Deno.readTextFileSync(path);
     return yaml.parse(content) as ciFileBlob[];
 }
 
-function composeTriggerTemplate(run: pipelineRun): triggerTemplate {
+function composeTriggerTemplate(run: pipelineRun, pr: prInfo): triggerTemplate {
     const ret: triggerTemplate = {
         apiVersion: 'triggers.tekton.dev/v1beta1',
         kind: 'TriggerTemplate',
@@ -29,9 +60,9 @@ function composeTriggerTemplate(run: pipelineRun): triggerTemplate {
             name: 'github-template-owner-repo-pr-12345',
             labels: {
                 type: 'github-pr',
-                prNum: 12345,
-                prOwner: 'PingCAP-QE',
-                prRepo: 'ee-ops',
+                prNum: pr.number,
+                prOwner: pr.baseOwner,
+                prRepo: pr.baseRepo,
             }
         },
         spec: {
@@ -53,7 +84,49 @@ function composeTriggerTemplate(run: pipelineRun): triggerTemplate {
     return ret;
 }
 
-function main({ input }: cliParams) {
+function composeTrigger(templateName: string, pr: prInfo): trigger {
+    const filterFormat = `
+        header.match('X-GitHub-Event', 'pull_request') &&
+        body.action in ['opened', 'synchronize'] &&
+        body.pull_request.base.user.login == %s &&
+        body.pull_request.base.repo.name == %s &&
+        body.pull_request.number == %d`;
+    const filter = sprintf(filterFormat, pr.baseOwner, pr.baseRepo, pr.number)
+    const ret: trigger = {
+        apiVersion: 'triggers.tekton.dev/v1beta1',
+        kind: 'Trigger',
+        metadata: {
+            name: 'github-template-owner-repo-pr-12345',
+            labels: {
+                type: 'github-pr',
+                prNum: pr.number,
+                prOwner: pr.baseOwner,
+                prRepo: pr.baseRepo,
+            }
+        },
+        spec: {
+            bindings: [{ ref: "github-pr" }],
+            template: {
+                ref: templateName
+            },
+            interceptors: [
+                {
+                    ref: { name: "cel" },
+                    params: [
+                        {
+                            name: "filter",
+                            value: filter
+                        }
+                    ]
+                }
+            ]
+        }
+    };
+
+    return ret;
+}
+
+function main({ input, pr }: cliParams) {
     if (!input) {
         input = DEFAULT_CI_MANIFEST_INPUT
     }
@@ -61,9 +134,11 @@ function main({ input }: cliParams) {
 
     pipelines.forEach(e => {
         const ePipelineRun = yaml.parse(e.content) as pipelineRun;
-        const eTriggerTemplate = composeTriggerTemplate(ePipelineRun);
+        const eTriggerTemplate = composeTriggerTemplate(ePipelineRun, pr);
+        const eTrigger = composeTrigger(eTriggerTemplate.metadata.name, pr)
 
         console.log(yaml.stringify(eTriggerTemplate))
+        console.log(yaml.stringify(eTrigger))
     });
 }
 
