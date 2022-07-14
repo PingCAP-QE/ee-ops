5b5b25cb237a7c3766ffb8e1c6d42902eee7ff4b
wuhuizuo <wuhuizuo@126.com>
2022-07-14 08:00:05 +0000

    feat(.ci/scripts): add change detect for mainifest composing

--- .ci/scripts/compose.ts
+++ .ci/scripts/compose.ts
@@ -1,169 +1,196 @@
 import * as yaml from "https://deno.land/std@0.147.0/encoding/yaml.ts";
 import * as flags from "https://deno.land/std@0.147.0/flags/mod.ts";
-import { sprintf } from "https://deno.land/std@0.147.0/fmt/printf.ts";
-import * as path from 'https://deno.land/std@0.147.0/path/mod.ts'
+import * as path from "https://deno.land/std@0.147.0/path/mod.ts";
 
 const DEFAULT_CI_MANIFEST_INPUT = "ci.yaml";
 const DEFAULT_CI_MANIFEST_OUTPUT = "tekton";
 
 interface ciFileBlob {
-    file_sha: string;
-    path: string;
-    content: string;
+  file_sha: string;
+  path: string;
+  content: string;
 }
 
 interface prInfo {
-    number: number;
-    user: string;
-    headOwner: string;
-    headRepo: string;
-    headRef: string;
-    baseOwner: string;
-    baseRepo: string;
-    baseRef: string;
+  number: number;
+  user: string;
+  headOwner: string;
+  headRepo: string;
+  headRef: string;
+  baseOwner: string;
+  baseRepo: string;
+  baseRef: string;
 }
 
 interface cliParams {
-    gitUrl: string;
-    input?: string; // default ci.yaml
-    output?: string; // default tekton.yaml
-    pr: prInfo;
+  gitUrl: string;
+  input?: string; // default ci.yaml
+  output?: string; // default tekton.yaml
+  pr: prInfo;
 }
 
 interface pipelineRun {
-    metadata: {
-        generateName: string;
-        [key: string]: any;
-    };
+  metadata: {
+    generateName: string;
     [key: string]: any;
+  };
+  [key: string]: any;
 }
 interface triggerTemplate {
-    kind: "TriggerTemplate";
-    metadata: {
-        name: string;
-        [key: string]: any;
-    };
-    spec: { [key: string]: any };
+  kind: "TriggerTemplate";
+  metadata: {
+    name: string;
     [key: string]: any;
+  };
+  spec: { [key: string]: any };
+  [key: string]: any;
 }
 interface trigger {
-    kind: "Trigger";
-    metadata: {
-        name: string;
-        [key: string]: any;
-    };
-    spec: { [key: string]: any };
+  kind: "Trigger";
+  metadata: {
+    name: string;
     [key: string]: any;
+  };
+  spec: { [key: string]: any };
+  [key: string]: any;
 }
 
 function parseRepo(gitUrl: string): { repo: string; owner: string } {
-    const [owner, repo] = new URL(gitUrl)
-        .pathname
-        .replace(/^(\/)/, "")
-        .replace(/(\.git)$/, "")
-        .split("/", 2);
-    return { owner, repo };
+  const [owner, repo] = new URL(gitUrl)
+    .pathname
+    .replace(/^(\/)/, "")
+    .replace(/(\.git)$/, "")
+    .split("/", 2);
+  return { owner, repo };
 }
 
 function readManifest(path: string) {
-    const content = Deno.readTextFileSync(path);
-    return yaml.parse(content) as ciFileBlob[];
+  const content = Deno.readTextFileSync(path);
+  return yaml.parse(content) as ciFileBlob[];
 }
 
 function composeTriggerTemplate(run: pipelineRun, pr: prInfo): triggerTemplate {
-    const ret: triggerTemplate = {
-        apiVersion: "triggers.tekton.dev/v1beta1",
-        kind: "TriggerTemplate",
-        metadata: {
-            name: `${pr.baseOwner.toLowerCase()}-${pr.baseRepo}-pr-${pr.number}`,
-            labels: {
-                'type': "github-pr",
-                'pr-num': `${pr.number}`,
-                'pr-owner': pr.baseOwner,
-                'pr-repo': pr.baseRepo,
-            },
+  const ret: triggerTemplate = {
+    apiVersion: "triggers.tekton.dev/v1beta1",
+    kind: "TriggerTemplate",
+    metadata: {
+      name: `${pr.baseOwner.toLowerCase()}-${pr.baseRepo}-pr-${pr.number}`,
+      labels: {
+        "type": "github-pr",
+        "pr-num": `${pr.number}`,
+        "pr-owner": pr.baseOwner,
+        "pr-repo": pr.baseRepo,
+      },
+    },
+    spec: {
+      params: [
+        {
+          name: "git-url",
+          description: "The git repository full url",
         },
-        spec: {
-            params: [
-                {
-                    name: "git-url",
-                    description: "The git repository full url",
-                },
-                {
-                    name: "git-revision",
-                    default: "main",
-                    description: "The git revision",
-                },
-            ],
-            resourcetemplates: [run],
+        {
+          name: "git-revision",
+          default: "main",
+          description: "The git revision",
         },
-    };
+      ],
+      resourcetemplates: [run],
+    },
+  };
 
-    return ret;
+  return ret;
 }
 
 function composeTrigger(templateName: string, pr: prInfo): trigger {
-    const filter = `\
+  const filter = `\
         header.match('X-GitHub-Event', 'pull_request') && \
         body.action in ['opened', 'synchronize'] && \
         body.pull_request.base.user.login == '${pr.baseOwner}' && \
         body.pull_request.base.repo.name == '${pr.baseRepo}' && \
         body.pull_request.number == ${pr.number}`;
 
-    const ret: trigger = {
-        apiVersion: "triggers.tekton.dev/v1beta1",
-        kind: "Trigger",
-        metadata: {
-            name: `${pr.baseOwner.toLowerCase()}-${pr.baseRepo}-pr-${pr.number}`,
-            labels: {
-                // TODO(wuhuizuo): label value should be 63 characters or less. 
-                'type': "github-pr",
-                'pr-num': `${pr.number}`,
-                'pr-owner': pr.baseOwner,
-                'pr-repo': pr.baseRepo,
-            },
-        },
-        spec: {
-            bindings: [{ ref: "github-pr" }],
-            template: {
-                ref: templateName,
-            },
-            interceptors: [{
-                ref: { name: "cel" },
-                params: [{ name: "filter", value: filter },],
-            }],
-        },
-    };
+  const ret: trigger = {
+    apiVersion: "triggers.tekton.dev/v1beta1",
+    kind: "Trigger",
+    metadata: {
+      name: `${pr.baseOwner.toLowerCase()}-${pr.baseRepo}-pr-${pr.number}`,
+      labels: {
+        // TODO(wuhuizuo): label value should be 63 characters or less.
+        "type": "github-pr",
+        "pr-num": `${pr.number}`,
+        "pr-owner": pr.baseOwner,
+        "pr-repo": pr.baseRepo,
+      },
+    },
+    spec: {
+      bindings: [{ ref: "github-pr" }],
+      template: {
+        ref: templateName,
+      },
+      interceptors: [{
+        ref: { name: "cel" },
+        params: [{ name: "filter", value: filter }],
+      }],
+    },
+  };
 
-    return ret;
+  return ret;
+}
+
+function composePipelineRun(run: pipelineRun, pr: prInfo): pipelineRun {
+  return Object.assign({}, run, {
+    spec: {
+      params: [
+        {
+          name: "git-url",
+          value: `https://github.com/${pr.headOwner}/${pr.headRepo}`,
+        },
+        { name: "git-revision", value: pr.headRef },
+      ],
+    },
+  });
 }
 
 function main({
-    input = DEFAULT_CI_MANIFEST_INPUT,
-    output = DEFAULT_CI_MANIFEST_OUTPUT,
-    gitUrl, pr
+  input = DEFAULT_CI_MANIFEST_INPUT,
+  output = DEFAULT_CI_MANIFEST_OUTPUT,
+  gitUrl,
+  pr,
 }: cliParams) {
-    const pipelines = readManifest(input);
+  const pipelines = readManifest(input);
 
-    if (!pr.baseOwner || !pr.baseRepo) {
-        const { owner, repo } = parseRepo(gitUrl);
-        pr.baseOwner = owner;
-        pr.baseRepo = repo;
-    }
-    pipelines.forEach((e) => {
-        const ePipelineRun = yaml.parse(e.content) as pipelineRun;
-        const eTriggerTemplate = composeTriggerTemplate(ePipelineRun, pr);
-        const eTrigger = composeTrigger(eTriggerTemplate.metadata.name, pr);
+  if (!pr.baseOwner || !pr.baseRepo) {
+    const { owner, repo } = parseRepo(gitUrl);
+    pr.baseOwner = owner;
+    pr.baseRepo = repo;
+  }
+  pipelines.forEach((e) => {
+    const ePipelineRun = yaml.parse(e.content) as pipelineRun;
+    const eTriggerTemplate = composeTriggerTemplate(ePipelineRun, pr);
+    const eTrigger = composeTrigger(eTriggerTemplate.metadata.name, pr);
+    const ePipelineOnceRun = composePipelineRun(ePipelineRun, pr);
 
-        const triggerTemplateYamlPath = path.join(output,
-            `${ePipelineRun.metadata.generateName}trigger-template.yaml`);
-        const triggerYamlPath = path.join(output,
-            `${ePipelineRun.metadata.generateName}trigger.yaml`);
+    const triggerTemplateYamlPath = path.join(
+      output,
+      `${ePipelineRun.metadata.generateName}trigger-template.yaml`,
+    );
+    const triggerYamlPath = path.join(
+      output,
+      `${ePipelineRun.metadata.generateName}trigger.yaml`,
+    );
+    const pipelineRunOnceYamlPath = path.join(
+      output,
+      `${ePipelineRun.metadata.generateName}pipeline-run.yaml`,
+    );
 
-        Deno.mkdir(output, { recursive: true });
-        Deno.writeTextFileSync(triggerTemplateYamlPath, yaml.stringify(eTriggerTemplate));
-        Deno.writeTextFileSync(triggerYamlPath, yaml.stringify(eTrigger));
-    });
+    Deno.mkdir(output, { recursive: true });
+    Deno.writeTextFileSync(
+      triggerTemplateYamlPath,
+      yaml.stringify(eTriggerTemplate),
+    );
+    Deno.writeTextFileSync(triggerYamlPath, yaml.stringify(eTrigger));
+    Deno.writeTextFileSync(pipelineRunOnceYamlPath, yaml.stringify(ePipelineOnceRun))
+  });
 }
 
 const cliArgs = flags.parse(Deno.args) as cliParams;
--- .ci/scripts/get.ts
+++ .ci/scripts/get.ts
@@ -2,121 +2,195 @@ import * as flags from "https://deno.land/std@0.147.0/flags/mod.ts";
 import * as yaml from "https://deno.land/std@0.147.0/encoding/yaml.ts";
 import * as base64 from "https://deno.land/std@0.147.0/encoding/base64.ts";
 import { sprintf } from "https://deno.land/std@0.147.0/fmt/printf.ts";
-import { Octokit, App } from "https://cdn.skypack.dev/octokit?dts";
+import { App, Octokit } from "https://cdn.skypack.dev/octokit?dts";
 
 const DEFAULT_CI_DIR = ".ci";
-const DEFAULT_CI_MANIFEST_OUTPUT = "ci.yaml"
+const DEFAULT_CI_MANIFEST_OUTPUT = "ci.yaml";
 
 /**
  * typescript style guide: https://google.github.io/styleguide/tsguide.html
  */
 
 interface cliParams {
-    appId: number;
-    privateKeyPath: string;
-    gitUrl: string;
-    sha: string;
-    ciDir?: string;
-    output?: string;
+  appId: number;
+  privateKeyPath: string;
+  gitUrl: string;
+  pullNumber?: number;
+  sha: string;
+  ciDir?: string;
+  output?: string;
 }
 
 interface ciFileBlob {
-    file_sha: string;
-    path: string;
-    content: string;
+  file_sha: string;
+  path: string;
+  content: string;
 }
 
-function parseRepo(gitUrl: string): { repo: string, owner: string } {
-    const [owner, repo] = new URL(gitUrl).
-        pathname.
-        replace(/^(\/)/, '').
-        replace(/(\.git)$/, '').
-        split('/', 2)
-    return { owner, repo };
+function parseRepo(gitUrl: string): { repo: string; owner: string } {
+  const [owner, repo] = new URL(gitUrl)
+    .pathname
+    .replace(/^(\/)/, "")
+    .replace(/(\.git)$/, "")
+    .split("/", 2);
+  return { owner, repo };
 }
 
 async function getRepoOctokit(
-    app: App,
-    owner: string,
-    repo: string,
+  app: App,
+  owner: string,
+  repo: string,
 ): Promise<Octokit> {
-    let ret: Octokit;
+  let ret: Octokit;
 
-    const fullRepoName = sprintf('%s/%s', owner, repo);
-    await app.eachRepository(({ octokit, repository }) => {
-        if (repository.full_name === fullRepoName) {
-            ret = octokit;
-        }
-    });
+  const fullRepoName = sprintf("%s/%s", owner, repo);
+  await app.eachRepository(({ octokit, repository }) => {
+    if (repository.full_name === fullRepoName) {
+      ret = octokit;
+    }
+  });
 
-    return ret;
+  return ret;
 }
 
 async function getCiFiles(
-    octokit: Octokit,
-    owner: string,
-    repo: string,
-    sha: string,
-    dir: string,
+  octokit: Octokit,
+  owner: string,
+  repo: string,
+  sha: string,
+  dir: string,
 ): Promise<ciFileBlob[]> {
-    // get tree path
-    const { data: { tree: dirs } } = await octokit.rest.git.getTree({
-        owner: owner,
-        repo: repo,
-        tree_sha: sha,
-    });
+  // get tree path
+  const { data: { tree: dirs } } = await octokit.rest.git.getTree({
+    owner: owner,
+    repo: repo,
+    tree_sha: sha,
+  });
 
-    // find dir
-    const matched = dirs.find(
-        (e: { path?: string; type?: string; sha: string }) => {
-            return e.path === dir && e.type === "tree";
-        },
-    );
+  // find dir
+  const matched = dirs.find(
+    (e: { path?: string; type?: string; sha: string }) => {
+      return e.path === dir && e.type === "tree";
+    },
+  );
 
-    if (!matched) {
-        return Promise.resolve([])
-    }
+  if (!matched) {
+    return Promise.resolve([]);
+  }
 
-    // get files
-    const { data: { tree } } = await octokit.rest.git.getTree({
-        owner: owner,
-        repo: repo,
-        tree_sha: matched.sha,
-        recursive: true,
-    });
+  // get files
+  const { data: { tree } } = await octokit.rest.git.getTree({
+    owner: owner,
+    repo: repo,
+    tree_sha: matched.sha,
+    recursive: true,
+  });
 
-    return await Promise.all(tree.
-        filter((f: { path: string; }) => f.path.endsWith(".yaml") || f.path.endsWith(".yml")).
-        map(async (f: { sha: string; path: string; }) => {
-            const { data: { content } } = await octokit.rest.git.getBlob({ owner, repo, file_sha: f.sha });
-            const file_content = new TextDecoder().decode(base64.decode(content));
-            return { path: f.path, file_sha: f.sha, content: file_content } as ciFileBlob;
-        })
-    );
+  return await Promise.all(
+    tree
+      .filter((f: { path: string }) =>
+        f.path.endsWith(".yaml") || f.path.endsWith(".yml")
+      )
+      .map(async (f: { sha: string; path: string }) => {
+        const { data: { content } } = await octokit.rest.git.getBlob({
+          owner,
+          repo,
+          file_sha: f.sha,
+        });
+        const file_content = new TextDecoder().decode(base64.decode(content));
+        return {
+          path: f.path,
+          file_sha: f.sha,
+          content: file_content,
+        } as ciFileBlob;
+      }),
+  );
+}
+
+async function getChangedCiFiles(
+  octokit: Octokit,
+  owner: string,
+  repo: string,
+  pull_number: number,
+  dir: string,
+): Promise<ciFileBlob[]> {
+  // TODO(wuhuizuo): get when changed files count more than 100.
+  const { data: files } = await octokit.rest.pulls.listFiles({
+    owner,
+    repo,
+    pull_number,
+    per_page: 100,
+  });
+
+  const textDecoder = new TextDecoder();
+
+  // TODO(wuhuizuo): consider .status == 'removed'
+  return await Promise.all(
+    files
+      .filter(({ filename: fn }) => {
+        return fn.startsWith(`${dir}/`) &&
+          (fn.endsWith(`.yaml`) || fn.endsWith(`.yml`));
+      })
+      .map(async ({ sha, filename, contents_url, status }) => {
+        const ref = new URL(contents_url).searchParams.get("ref");
+        const { data: { content } } = await octokit.rest.repos.getContent({
+          owner,
+          repo,
+          ref,
+          path: filename,
+        });
+        return {
+          file_sha: sha,
+          path: filename,
+          content: textDecoder.decode(base64.decode(content)),
+        };
+      }),
+  );
 }
 
 async function main({
-    ciDir = DEFAULT_CI_DIR,
-    output = DEFAULT_CI_MANIFEST_OUTPUT,
-    appId, privateKeyPath, gitUrl, sha,
+  ciDir = DEFAULT_CI_DIR,
+  output = DEFAULT_CI_MANIFEST_OUTPUT,
+  appId,
+  privateKeyPath,
+  gitUrl,
+  sha,
+  pullNumber,
 }: cliParams) {
-    const { owner, repo } = parseRepo(gitUrl);
+  const { owner, repo } = parseRepo(gitUrl);
 
-    // decode private key from base64 encoded string
-    const privateKey = await Deno.readTextFile(privateKeyPath);
-    const app = new App({ appId, privateKey });
-    const { data: { slug } } = await app.octokit.rest.apps.getAuthenticated();
-    console.debug({ slug, owner, repo });
+  // decode private key from base64 encoded string
+  const privateKey = await Deno.readTextFile(privateKeyPath);
+  const app = new App({ appId, privateKey });
+  const { data: { slug } } = await app.octokit.rest.apps.getAuthenticated();
+  console.debug({ slug, owner, repo });
 
-    const files = await getCiFiles(
-        await getRepoOctokit(app, owner, repo),
-        owner,
-        repo,
-        sha,
-        ciDir,
+  const octokit = await getRepoOctokit(app, owner, repo);
+
+  let files: ciFileBlob[];
+  if (pullNumber) {
+    files = await getChangedCiFiles(
+      octokit,
+      owner,
+      repo,
+      pullNumber,
+      ciDir,
     );
+  } else if (sha) {
+    files = await getCiFiles(
+      octokit,
+      owner,
+      repo,
+      sha,
+      ciDir,
+    );
+  }
 
-    await Deno.writeFile(output, new TextEncoder().encode(yaml.stringify(files)))
+  files.forEach((f) => console.log(f));
+  await Deno.writeFile(
+    output,
+    new TextEncoder().encode(yaml.stringify(files)),
+  );
 }
 
 const cliArgs = flags.parse(Deno.args) as cliParams;
