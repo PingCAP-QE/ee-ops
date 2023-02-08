df5c62ca04aa9642e3bfbd702fc026bd779f198f
wuhuizuo <wuhuizuo@126.com>
2023-02-09 01:02:47 +0800

    Revert "feat(apps/prod/jenkins/post/tidb): optimize the bazel `--jobs` option (#401)"
    This reverts commit 57c9161f8f7c4d14d4d32af0b99349d4b30f6ddb.

--- apps/prod/jenkins/post/tidb/files/bazel-prepare-in-container.sh
+++ apps/prod/jenkins/post/tidb/files/bazel-prepare-in-container.sh
@@ -27,9 +27,6 @@
 #              resource: requests.memory
 # 4.2 (optional) need a PV or PVC mounted at /share/.cache/bazel-repository-cache with `ReadWriteMany`` mode.
 
-# default job max bazel memory is 8GB in tidb.
-DEFAULT_BAZEL_JVM_MAX_MEM_MB=8192
-
 if which bazel; then
     echo "has bazel tool."
 else
@@ -37,25 +34,22 @@ else
     exit 0
 fi
 
-mkdir /bazel-out-overlay/upper /bazel-out-overlay/work
-sudo mount -t overlay overlay /home/jenkins/.tidb/tmp -o lowerdir=/bazel-out-lower,upperdir=/bazel-out-overlay/upper,workdir=/bazel-out-overlay/work
+mkdir /bazel-out-overlay/upper /bazel-out-overlay/work; 
+sudo mount -t overlay overlay /home/jenkins/.tidb/tmp -o lowerdir=/bazel-out-lower,upperdir=/bazel-out-overlay/upper,workdir=/bazel-out-overlay/work;
 
 # generate file $HOME/.bazelrc
 # ref: https://docs.bazel.build/versions/5.3.1/user-manual.html
-: >~/.bazelrc
+:> ~/.bazelrc
 
 # fix bazel OOM in container.
 if [ -d /etc/containerinfo ]; then
     cpu_limit=$(cat /etc/containerinfo/cpu_limit)
     mem_limit=$(cat /etc/containerinfo/mem_limit)
-    mem_limit=$(((mem_limit / 1048576) * 9 / 10))
-
-    job_limit=$((mem_limit / DEFAULT_BAZEL_JVM_MAX_MEM_MB))
-    job_limit=$((job_limit < cpu_limit ? job_limit : cpu_limit))
-    echo "build --local_ram_resources=${mem_limit} --local_cpu_resources=${cpu_limit} --jobs=${job_limit}" >>~/.bazelrc
+    mem_limit=$(((mem_limit / 1048576) * 9 / 10 ))
+    echo "build --local_ram_resources=${mem_limit} --local_cpu_resources=${cpu_limit} --jobs=${cpu_limit}" >> ~/.bazelrc
 fi
 
 # set repository cache: https://docs.bazel.build/versions/5.3.1/guide.html#the-repository-cache
 if [ -d /share/.cache/bazel-repository-cache ]; then
-    echo "build --repository_cache=/share/.cache/bazel-repository-cache" >>~/.bazelrc
+    echo "build --repository_cache=/share/.cache/bazel-repository-cache" >> ~/.bazelrc
 fi
