c6707b17f107b1eb1984a1162c04d8ed2d86e9d5
wuhuizuo <wuhuizuo@126.com>
2024-03-21 21:03:16 +0800

    test(apps/prod/tekton/configs/triggers): add deno script to send github events
    support event types:
    - github push event
    - github tag create event
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

---
+++ apps/prod/tekton/configs/triggers/tests/test-github-events.ts
@@ -0,0 +1,97 @@
+#!/usr/bin/env deno run --allow-net
+
+import { parseArgs } from "https://deno.land/std@0.220.1/cli/parse_args.ts";
+
+interface Payload {
+  ref: string;
+  before: string;
+  after: string;
+  ref_type: string;
+  repository: {
+    name: string;
+    clone_url: string;
+    owner: {
+      login: string;
+    };
+  };
+}
+
+async function getCommitSha(
+  owner: string,
+  repo: string,
+  ref: string,
+): Promise<string> {
+  const apiUrl = `https://api.github.com/repos/${owner}/${repo}/commits/${ref}`;
+  const response = await fetch(apiUrl);
+  const commit = await response.json();
+  return commit.sha;
+}
+
+async function generateEventPayload(
+  gitUrl: string,
+  ref: string,
+): Promise<Payload> {
+  const url = new URL(gitUrl);
+  const owner = url.pathname.split("/")[1];
+  const repoName = url.pathname.split("/").pop()?.replace(/\.git$/, "");
+  const isTag = ref.startsWith("refs/tags/");
+  const lastCommitSha = await getCommitSha(owner, repoName!, ref);
+
+  return {
+    ref: ref,
+    before: "0000000000000000000000000000000000000000",
+    after: lastCommitSha,
+    ref_type: isTag ? "tag" : "branch",
+    repository: {
+      name: repoName!,
+      clone_url: gitUrl,
+      owner: {
+        login: owner,
+      },
+    },
+  };
+}
+
+async function sendEvent(
+  gitUrl: string,
+  ref: string,
+  eventUrl: string,
+): Promise<void> {
+  const payload = await generateEventPayload(gitUrl, ref);
+  const eventType = payload.ref_type === "branch" ? "push" : "create";
+
+  console.debug(payload);
+  const response = await fetch(eventUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-GitHub-Event": eventType,
+    },
+    body: JSON.stringify(payload),
+  });
+
+  if (!response.ok) {
+    console.error(`Request failed with status ${response.status}`);
+    Deno.exit(1);
+  }
+
+  console.log("Request succeeded");
+}
+
+async function main() {
+  const args = parseArgs(Deno.args);
+  const gitUrl = args.url;
+  const ref = args.ref || "refs/heads/master";
+  const eventUrl = args.eventUrl || "https://example.com";
+
+  if (!gitUrl) {
+    console.error(
+      "Usage: deno run script.ts --url <git-url> [--ref <ref>] [--eventUrl <event-url>]",
+    );
+    Deno.exit(1);
+  }
+
+  await sendEvent(gitUrl, ref, eventUrl);
+}
+
+await main();
